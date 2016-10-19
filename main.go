36f8449214c9b66236e52f10ae0dd79bfa17defd
maclinuxer <maclinuxer@163.com>
2016-10-19 21:39:23 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func foo() {
+	fmt.Println("start a new line")
+	fmt.Println("adsadsfasdfasfas")
+}
+
+func main() {
+	var x = 10
+	fmt.Printf("x_value: %d, x_addr: %p\n", x, &x)
+
+	var px = &x
+	fmt.Printf("px_value: %d, px_addr: %p\n", *px, px)
+
+	s := "good bye"
+	var p *string = &s
+	*p = "ciao"
+	fmt.Printf("here is the pointer p: %p\n", p)
+	fmt.Printf("here is the string *p: %s\n", *p)
+	fmt.Printf("here is the string s: %s\n", s)
+	fmt.Print("aldfjasldjflasdjfl")
+
+}
